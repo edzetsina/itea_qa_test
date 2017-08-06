e075a92a7133646d65270ddcb3c711d3e27c55b8
edzetsina <e.dzetsina@gmail.com>
2017-08-06 16:29:56 +0300

    Completed task

--- src/test/java/Page/GooglePage.java
+++ src/test/java/Page/GooglePage.java
@@ -1,55 +1,134 @@
 package Page;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Google Page
+ */
 public class GooglePage {
+    /**
+     * WebDriver instance
+     *
+     * @param webDriver WebDriver instance
+     */
+    public GooglePage(WebDriver webDriver) {
+        PageFactory.initElements(webDriver, this);
+    }
 
     public WebDriver webDriver;
 
-    public GooglePage(WebDriver webDriver) {
-      PageFactory.initElements(webDriver, this);
-    }
-
-
-    @FindBy (xpath = "//input[@id='gs_htif0']")
+    /**
+     * Identify search field
+     */
+    @FindBy(id = "lst-ib")
     private WebElement searchField;
-    @FindBy (xpath = "//*[@name='btnK']")
-    private  WebElement searchButton;
-    @FindBy (xpath = ".//*[@id='res']")
+    /**
+     * Identify Search button element
+     */
+    @FindBy(name = "btnK")
+    private WebElement searchButton;
+    /**
+     * Identify search results elements
+     */
+    @FindBy(xpath = "//*[@class='rc']")
     private WebElement searchResults;
+    /**
+     * Identify list of elements search results
+     */
     @FindBy(xpath = ".//*[@id='res']//*[@class='g']")
     private List<WebElement> searchResultsList;
-    @FindBy (xpath = ".//*[@id='nav']//td[3]")
+    /**
+     * Identify second result page
+     */
+    @FindBy(xpath = ".//*[@id='nav']//td[3]")
     private WebElement secondSearchPage;
 
+    /**
+     * @param searchKeys String search text
+     * @return Google Page
+     */
     public GooglePage searchResults(String searchKeys) {
         searchField.sendKeys(searchKeys);
         searchButton.click();
-        return this;
+        return PageFactory.initElements(webDriver, GooglePage.class);
     }
+
     public boolean isPageLoaded() {
         return searchResults.isDisplayed();
     }
 
+    /**
+     * Gets count of search results at the page
+     *
+     * @return int quantity of results
+     */
     public int getSearchResultsCount() {
         return searchResultsList.size();
     }
 
+    /**
+     * Method which open second search results page
+     */
     public GooglePage switchToSecondSearchPage() {
         secondSearchPage.click();
         return this;
     }
-    public boolean isAllSearchResultsContainKeywords(String keyword) {
-        for (WebElement iteaSearch : searchResultsList) {
-            if (!iteaSearch.getText().equalsIgnoreCase(keyword)) {
-                return false;
-            }
+
+    /**
+     * Gets List of desired search results details elements
+     *
+     * @param searchText String "searchHeaders", "searchAddresses"
+     * @return listDetails List of WebElements
+     */
+    public List<String> getSearchDetails(String searchText) {
+        List<String> listDetails = new ArrayList<>();
+        String elementXpath = getSearchDetailsXpath(searchText);
+        for (WebElement resultElement : searchResultsList) {
+            String searchDetailsText = resultElement.findElement(By.xpath(elementXpath)).getText();
+            listDetails.add(searchDetailsText);
         }
-        return true;
+        return listDetails;
+    }
+
+    /**
+     * gets Xpath string to get search details
+     *
+     * @param searchText String "searchHeaders", "searchAddresses"
+     * @return String with xpath to get search details elements
+     */
+    public String getSearchDetailsXpath(String searchText) {
+
+        switch (searchText.toLowerCase()) {
+            case "searchheaders":
+                return ".//h3//a";
+            case "searchaddresses":
+                return ".//cite";
+            default:
+                return "";
+        }
+    }
+
+    /**
+     * Checks if all of results on the page contains searched keywords
+     *
+     * @param searchText String keywords
+     * @return true if all result contains search keywords
+     */
+    public boolean isAllResultContains(String searchText) {
+        List<String> listHeaders = getSearchDetails("searchHeaders");
+        List<String> listAddresses = getSearchDetails("searchAddresses");
+        boolean result = false;
+        for (int i = 0; i < searchResultsList.size(); i++) {
+            boolean isAddressElementContainsSearchTerm = listAddresses.get(i).toLowerCase().contains(searchText.toLowerCase());
+            boolean isHeaderElementContainsSearchTerm = listHeaders.get(i).toLowerCase().contains(searchText.toLowerCase());
+            result = isAddressElementContainsSearchTerm || isHeaderElementContainsSearchTerm;
+        }
+        return result;
     }
 }
--- src/test/java/Test/ITEATests.java
+++ src/test/java/Test/ITEATests.java
@@ -1,51 +1,48 @@
 package Test;
 
 import Page.GooglePage;
-import io.github.bonigarcia.wdm.ChromeDriverManager;
 import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
-public class ITEATests {
+public class IteaTests {
     public WebDriver webDriver;
     public String searchKeys = "ITEA";
 
+    /**
+     * Initialises FirefoxDriver and navigates to Google page
+     */
     @Parameters({"BrowserType"})
-
-    @BeforeMethod
-    public void beforeMethod(@Optional("firefox") String BrowserType) throws InterruptedException {
-        if (BrowserType.toLowerCase().equals("chrome")) {
-            ChromeDriverManager.getInstance().setup();
-            webDriver = new ChromeDriver();
-        }
-        if (BrowserType.toLowerCase().equals("firefox")) {
-            FirefoxDriverManager.getInstance().setup();
-            webDriver = new FirefoxDriver();
-        }
+    @BeforeClass
+    public void beforeClass(@Optional("firefox") String BrowserType) {
+        FirefoxDriverManager.getInstance().setup();
+        webDriver = new FirefoxDriver();
         webDriver.navigate().to("https://www.google.com.ua/");
     }
 
     /**
      * Kills WebDriver instance
      */
-    @AfterMethod
-    public void afterMethod() {
+    @AfterClass
+    public void afterClass() {
         webDriver.quit();
     }
 
-@Test
+    @Test
     public void testSearchResults() throws InterruptedException {
+     String searchText = "ITEA";
      GooglePage googlePage = new GooglePage(webDriver);
      googlePage.searchResults(searchKeys);
+
      Assert.assertTrue(googlePage.isPageLoaded(), "Search results page is not loaded");
-     int resultsCount = googlePage.getSearchResultsCount();
-     Assert.assertEquals(resultsCount, 10, "Results count doesn't match real");
+     Assert.assertEquals(googlePage.getSearchResultsCount(), 7, "Results count doesn't match real");
+     Assert.assertTrue(googlePage.isAllResultContains(searchText), "Not every result contains search term");
 
      googlePage.switchToSecondSearchPage();
-     Assert.assertEquals(resultsCount, 10, "Results count doesn't match real");
-
+     Assert.assertTrue(googlePage.isPageLoaded(), "Search results page is not loaded");
+     Assert.assertEquals(googlePage.getSearchResultsCount(), 10, "Results count doesn't match real");
+     Assert.assertTrue(googlePage.isAllResultContains(searchText), "Not every result contains search term");
     }
 }
